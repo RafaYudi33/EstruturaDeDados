88bcb3eb5b2534093934eb3b6250b5dbf4aea0ef
Rafael Yudi <rafael.tanaka@unesp.br>
2023-04-06 16:38:41 -0300

    funçoes pilha estatica

--- ED I/Pilhas/pilhasSimplesEstatica/pilhaEstatica.c
+++ ED I/Pilhas/pilhasSimplesEstatica/pilhaEstatica.c
@@ -8,20 +8,48 @@
 struct Pilha{
     int vPilha[MAX];
     int topo;  
+    int tam; 
 };
 typedef struct Pilha Pilha; 
 
 void inicializaLista(Pilha *pilha){
     pilha->topo = 0; 
+    pilha->tam = 0;
 
 }
 
-bool 
+bool verificaVazia(Pilha *pilha){
+    if(pilha->tam == 0){
+        printf("A pilha esta vazia");
+        return true;
+    }
+}
+
+bool pilhaCheia
+
+void push (Pilha *pilha, int elemento){
+    pilha->vPilha[pilha->topo] = elemento; 
+    pilha->topo++; 
+}
+
+void imprimePilha(Pilha *pilha){
+    int aux = pilha->topo; 
+    
+    while(aux != MAX+1){
+        printf("%d ", pilha->vPilha[aux]);
+        aux++;  
+    }
+
+}
 
 
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+
+int main(int argc, char const *argv[]){
+
+
+    Pilha pilha; 
+    inicializaLista(&pilha); 
+    verificaVazia(&pilha); 
+
 }
