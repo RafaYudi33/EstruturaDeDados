63f26b0d643332cc128fe790698d0d41683b7c99
Rafael Yudi <rafael.tanaka@unesp.br>
2023-04-25 21:34:14 -0300

    inserção matriz esparsa

--- ED I/ListasCruzadas/listasCruzadas.c
+++ ED I/ListasCruzadas/listasCruzadas.c
@@ -36,11 +36,60 @@ void inserir(MatrizEsparsa *matriz, int novoElemento, int lin, int col){
     novoNo->coluna = col; 
     novoNo->baixo = NULL; 
     novoNo->direita = NULL;
-    pontLinha = matriz->linha[lin]; 
-    pontCol = matriz->coluna[col]; 
+    pontLinha = matriz->linha[lin]; // aponta para o primeiro elemento da linha que vai ocorrer a inserção
+    pontCol = matriz->coluna[col]; // aponta para o primeiro elemento da coluna que vai ocorrer a inserção
 
-    
+    //inserção ordenada na linha considerando o valor da coluna 
+    if(pontCol == NULL){ // se nao tiver nenhum elemento na coluna apenas inserir na coluna
+        matriz->coluna[col] = novoNo; 
+    }else{
+        ant = NULL; 
 
+        while((pontLinha->coluna < col) && (pontLinha != NULL)){//pontLinha->coluna é a coluna do primeiro elemento dessa linha
+            ant = pontLinha; 
+            pontLinha = pontLinha->direita;     
+        } 
+
+        if(pontLinha != NULL){// se a linha nao for vazia
+            if(pontLinha->coluna == col){
+                printf("Ja existe um elemento nesta coluna!"); 
+            }else if(ant == NULL){// se for no inicio
+                novoNo->direita = matriz->linha[lin]; 
+                matriz->linha[lin]; 
+            }else if((ant != NULL) && (pontLinha!=NULL)){//se for no meio
+                novoNo->direita = ant->direita; 
+                ant->direita = novoNo; 
+            }else{// se for no final
+                ant->direita = novoNo; 
+            }   
+        }
+    }
+
+    //inserção ordenada na coluna considerando o valor da linha    
+    if(pontLinha == NULL){
+        matriz->linha[lin] = novoNo; 
+    }else{
+        ant = NULL; 
+
+        while((pontCol->linha < lin) && (pontCol != NULL)){
+            ant = pontCol; 
+            pontCol = pontCol->baixo;
+        }
+
+        if(pontCol != NULL){
+            if(pontCol->linha == lin){
+                printf("Ja existe um elemento nessa linha"); 
+            }else if(ant = NULL){// se for no inicio
+                novoNo->baixo = matriz->coluna[col]; 
+                matriz->coluna[col] = novoNo; 
+            }else if((ant != NULL)&&(pontCol != NULL)){// se for no meio
+                novoNo->direita = ant->direita; 
+                ant->direita = novoNo; 
+            }else{// se for no final
+                ant->direita = novoNo; 
+            }
+        }
+    }
 
 }
 
